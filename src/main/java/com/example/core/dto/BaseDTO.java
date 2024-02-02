0ce242d1f8f9ba928dad0c19b27fc47f908dba29
hsl24 <hslee@skyware.co.kr>
2024-02-02 10:43:04 +0900

    Feat : BaseDTO 추가

---
+++ src/main/java/com/example/core/dto/BaseDTO.java
@@ -0,0 +1,14 @@
+package com.example.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseDTO {
+
+    private String createDate;
+    private String updateDate;
+}
