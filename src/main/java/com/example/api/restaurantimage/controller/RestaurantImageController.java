c6e2638243168e39c83193b8fe6b18dd54a511af
swkim <swkim@skyware.co.kr>
2024-03-06 17:01:14 +0900

    Feat: 식당 이미지 생성 시 dto를 받도록 수정

--- src/main/java/com/example/api/restaurantimage/controller/RestaurantImageController.java
+++ src/main/java/com/example/api/restaurantimage/controller/RestaurantImageController.java
@@ -1,5 +1,8 @@
 package com.example.api.restaurantimage.controller;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+
 import com.example.api.owner.dto.Owner;
 import com.example.api.restaurant.dto.RestaurantImageType;
 import com.example.api.restaurantimage.RestaurantImageService;
@@ -7,16 +10,23 @@ import com.example.api.restaurantimage.dto.AddRestaurantImagesDTO;
 import com.example.core.web.security.login.LoginMember;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,18 +39,21 @@ public class RestaurantImageController {
 
     private final RestaurantImageService restaurantImageService;
 
-    @PostMapping(consumes = {"multipart/form-data"})
     @Operation(summary = "식당 이미지들 추가", description = "식당의 이미지들을 추가하는 기능입니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "식당 이미지들 생성 성공"),
+    })
+    @PostMapping(consumes = {MULTIPART_FORM_DATA_VALUE, APPLICATION_JSON_VALUE})
     public ResponseEntity<Void> addRestaurantImages(@Parameter(hidden = true) @LoginMember Owner owner,
                                                     @PathVariable long restaurantId,
-                                                    @Parameter @RequestParam List<RestaurantImageType> restaurantImageTypes,
-                                                    @Parameter(description = "이미지 형식의 파일만 가능, 최소 1개 이상") @RequestPart List<MultipartFile> files)
+                                                    @Parameter @RequestPart AddRestaurantImagesReq addRestaurantImagesReq,
+                                                    @RequestPart List<MultipartFile> files)
             throws IOException {
 
         AddRestaurantImagesDTO dto = AddRestaurantImagesDTO.builder()
                 .restaurantId(restaurantId)
                 .files(files)
-                .restaurantImageTypes(restaurantImageTypes)
+                .restaurantImageTypes(addRestaurantImagesReq.restaurantImageTypes)
                 .build();
         restaurantImageService.addRestaurantImages(dto, owner.getOwnerId());
 
@@ -48,5 +61,15 @@ public class RestaurantImageController {
         return ResponseEntity.created(uri).build();
     }
 
-    // todo requestBody로 List<String> 받을 수 있는 지 테스트해보기
+    @Schema(description = "식당 이미지들 생성 요청값")
+    @EqualsAndHashCode
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AddRestaurantImagesReq {
+
+        @NotNull
+        @Schema(description = "식당 이미지 타입", example = "대표 이미지 - REPRESENTATIVE, 일반 이미지 - NORMAL")
+        private List<RestaurantImageType> restaurantImageTypes;
+    }
 }
