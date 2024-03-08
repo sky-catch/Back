41cd6d5bc2b0f1cae2bd10b4ce67f77b9d44aff4
swkim <swkim@skyware.co.kr>
2024-03-08 15:15:04 +0900

    Refactor: 인라인 함수화

--- src/main/java/com/example/core/web/security/login/LoginMemberArgumentResolver.java
+++ src/main/java/com/example/core/web/security/login/LoginMemberArgumentResolver.java
@@ -6,27 +6,26 @@ import com.example.api.member.UsersMapper;
 import com.example.api.owner.OwnerMapper;
 import com.example.api.owner.dto.Owner;
 import com.example.core.exception.SystemException;
-import com.example.core.web.security.jwt.JWTProvider;
-import com.example.core.web.security.jwt.JWTUtils;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+// filter 처리 후 진입
+@Slf4j
 @Component
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
 
-    private final JWTProvider jwtProvider;
     private final Map<Class<?>, UsersMapper<?>> usersMapperMap;
 
-    public LoginMemberArgumentResolver(JWTProvider jwtProvider, MemberMapper memberMapper, OwnerMapper ownerMapper) {
-        this.jwtProvider = jwtProvider;
+    public LoginMemberArgumentResolver(MemberMapper memberMapper, OwnerMapper ownerMapper) {
         this.usersMapperMap = new HashMap<>();
         usersMapperMap.put(MemberDTO.class, memberMapper);
         usersMapperMap.put(Owner.class, ownerMapper);
@@ -42,20 +41,30 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
                                   NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory)
             throws Exception {
 
+        if (isNotAuthenticated()) {
+            throw new SystemException("토큰 인증에 실패하였습니다.");
+        }
+
         LoginMember loginMemberAnnotation = methodParameter.getParameterAnnotation(LoginMember.class);
         if (!loginMemberAnnotation.required()) {
             return null;
         }
 
-        HttpServletRequest request = nativeWebRequest.getNativeRequest(HttpServletRequest.class);
-
         Class<?> parameterType = methodParameter.getParameterType();
 
-        return Optional.ofNullable(request.getHeader(JWTUtils.AUTHORIZATION_HEADER))
-                .map(authorization -> authorization.split(JWTUtils.TOKEN_PREFIX)[1])
-                .map(jwtProvider::getMemberEmail)
-                .map(email -> usersMapperMap.get(parameterType).findByEmail(email)
-                        .orElseThrow(() -> new SystemException("존재하지 않는 사용자입니다.")))
-                .orElseThrow(() -> new SystemException("권한이 없습니다."));
+        log.info("parameterType = {}", parameterType);
+
+        String email = getAuthenticatedUserEmail();
+        return usersMapperMap.get(parameterType).findByEmail(email)
+                .orElseThrow(() -> new SystemException("존재하지 않는 사용자입니다."));
+    }
+
+    private boolean isNotAuthenticated() {
+        return !SecurityContextHolder.getContext().getAuthentication().isAuthenticated();
+    }
+
+    private String getAuthenticatedUserEmail() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userDetails.getUsername();
     }
 }
