654a316ce9e0ef31667b5129e61fcb6af30c5ab6
swkim <swkim@skyware.co.kr>
2024-03-12 16:08:07 +0900

    Build: 파라미터 스웨거 설정 추가

--- src/main/java/com/example/api/mydining/MyDiningController.java
+++ src/main/java/com/example/api/mydining/MyDiningController.java
@@ -26,7 +26,7 @@ public class MyDiningController {
     @GetMapping("/my/{status}")
     @Operation(summary = "나의 예약 조회", description = "방문 상태로 내가 예약한 가게를 조회할 수 있습니다.")
     public List<GetReservationRes> getMyReservations(@Parameter(hidden = true) @LoginMember MemberDTO memberDTO,
-                                                     @PathVariable ReservationStatus status) {
+                                                     @Parameter(description = "방문 상태") @PathVariable ReservationStatus status) {
 
         GetMyReservationDTO dto = GetMyReservationDTO.builder()
                 .memberId(memberDTO.getMemberId())
