fa7f54201a346e5a796d7dee91fcec74dd150f7f
swkim <swkim@skyware.co.kr>
2024-03-14 15:57:30 +0900

    Test: 테스트 케이스 삭제 코드 추가

--- src/test/java/com/example/api/restaurant/RestaurantServiceTest.java
+++ src/test/java/com/example/api/restaurant/RestaurantServiceTest.java
@@ -23,6 +23,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -65,6 +66,11 @@ class RestaurantServiceTest {
                 .address("address")
                 .detailAddress("detailAddress")
                 .build();
+    }
+
+    @AfterEach
+    void cleanup() {
+        restaurantMapper.deleteAll();
     }
 
     @Test
