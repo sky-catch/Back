0de5ffc0e12371078d1d2c35d0326ec587975ebf
swkim <swkim@skyware.co.kr>
2024-03-29 17:47:05 +0900

    Swagger: 예약 생성 스웨거 내용 추가

--- src/main/java/com/example/api/reservation/ReservationController.java
+++ src/main/java/com/example/api/reservation/ReservationController.java
@@ -9,14 +9,18 @@ import com.example.api.reservation.dto.request.GetAvailableTimeSlotsReq;
 import com.example.core.web.security.login.LoginMember;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,7 +32,11 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @PostMapping("/{restaurantId}")
-    @Operation(summary = "예약 생성", description = "방문일과 방문 시간에 식당 예약하는 API입니다.")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "예약 생성", description = "방문일과 방문 시간에 식당 예약을 생성하는 API입니다. 예약 생성 요청 시 결제를 생성하고 예약을 생성합니다.")
+    @ApiResponses(
+            @ApiResponse(responseCode = "201", description = "예약 생성 성공")
+    )
     public void createReservation(@Parameter(hidden = true) @LoginMember MemberDTO loginMember,
                                   @PathVariable long restaurantId,
                                   @Valid @RequestBody CreateReservationReq req) {
