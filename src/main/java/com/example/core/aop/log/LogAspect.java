d264a6a5cd5b42b28431ed9527088d39b6f6173b
blackarea <supersute142@gmail.com>
2024-03-31 19:49:49 +0900

    Feat : 로그 표시 및 파일 설정

---
+++ src/main/java/com/example/core/aop/log/LogAspect.java
@@ -0,0 +1,31 @@
+package com.example.core.aop.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+@Aspect
+public class LogAspect {
+
+    @Pointcut("execution(* *..*Controller.*(..))")
+    public void controllerLog(){}
+
+    @Around("controllerLog()")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        Signature signature = joinPoint.getSignature();
+        log.info( "[REQUEST] {} - {}.{}" ,request.getRequestURI(), signature.getDeclaringType().getSimpleName(), signature.getName());
+        return joinPoint.proceed();
+    }
+
+}
