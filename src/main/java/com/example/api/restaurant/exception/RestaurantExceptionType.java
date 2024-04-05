7601d66f5bf968bcc2eb21c5f9bb69a875ae7a6d
swkim <swkim@skyware.co.kr>
2024-04-05 13:54:45 +0900

    Feat: 식당 저장 시 식당의 식당 저장 컬럼 값 변경되도록 수정

--- src/main/java/com/example/api/restaurant/exception/RestaurantExceptionType.java
+++ src/main/java/com/example/api/restaurant/exception/RestaurantExceptionType.java
@@ -13,6 +13,7 @@ public enum RestaurantExceptionType implements CommonExceptionType {
     NOT_OWNER("식당 주인이 아닙니다.", HttpStatus.BAD_REQUEST),
     CAN_CREATE_ONLY_ONE("식당은 한 개만 생성할 수 있습니다.", HttpStatus.BAD_REQUEST),
     NOT_UNIQUE_NAME("식당 이름이 중복됩니다.", HttpStatus.BAD_REQUEST),
+    NOT_VALID_SAVED_COUNT("잘못된 식당 저장 개수입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     ;
 
     private final String message;
--- src/test/java/com/example/api/savedrestaurant/SavedRestaurantServiceTest.java
+++ src/test/java/com/example/api/savedrestaurant/SavedRestaurantServiceTest.java
@@ -1,9 +1,14 @@
 package com.example.api.savedrestaurant;
 
+import static com.example.api.restaurant.exception.RestaurantExceptionType.NOT_VALID_SAVED_COUNT;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.example.api.restaurant.RestaurantMapper;
+import com.example.api.restaurant.dto.RestaurantDTO;
 import com.example.core.exception.SystemException;
+import java.time.LocalTime;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,15 +29,35 @@ class SavedRestaurantServiceTest {
 
     private final SavedRestaurantService savedRestaurantService;
     private final SavedRestaurantMapper savedRestaurantMapper;
+    private final RestaurantMapper restaurantMapper;
+
+    private RestaurantDTO testRestaurant;
 
     @BeforeEach
     void init() {
         savedRestaurantMapper.deleteAll();
+
+        testRestaurant = RestaurantDTO.builder()
+                .ownerId(1L)
+                .name("name")
+                .category("category")
+                .content("content")
+                .phone("phone")
+                .tablePersonMax(4)
+                .tablePersonMin(2)
+                .openTime(LocalTime.now())
+                .lastOrderTime(LocalTime.now())
+                .closeTime(LocalTime.now())
+                .address("address")
+                .detailAddress("detailAddress")
+                .build();
+        restaurantMapper.save(testRestaurant);
     }
 
     @AfterEach
     void cleanup() {
         savedRestaurantMapper.deleteAll();
+        restaurantMapper.deleteAll();
     }
 
     @Test
@@ -41,7 +66,7 @@ class SavedRestaurantServiceTest {
         // given
         CreateSavedRestaurantDTO dto = CreateSavedRestaurantDTO.builder()
                 .memberId(1L)
-                .restaurantId(1L)
+                .restaurantId(testRestaurant.getRestaurantId())
                 .build();
         long before = savedRestaurantMapper.findAll().size();
 
@@ -50,7 +75,11 @@ class SavedRestaurantServiceTest {
 
         // then
         long after = savedRestaurantMapper.findAll().size();
-        assertEquals(before + 1, after);
+        RestaurantDTO actual = restaurantMapper.findById(testRestaurant.getRestaurantId()).get();
+        assertAll(() -> {
+            assertEquals(before + 1, after);
+            assertEquals(testRestaurant.getSavedCount() + 1, actual.getSavedCount());
+        });
     }
 
     @Test
@@ -75,15 +104,17 @@ class SavedRestaurantServiceTest {
         // given
         long before = savedRestaurantMapper.findAll().size();
 
-        CreateSavedRestaurantDTO createSavedRestaurantDTO = CreateSavedRestaurantDTO.builder()
+        SavedRestaurantDTO savedRestaurant = SavedRestaurantDTO.builder()
                 .memberId(1L)
-                .restaurantId(1L)
+                .restaurantId(testRestaurant.getRestaurantId())
                 .build();
-        savedRestaurantService.createSavedRestaurant(createSavedRestaurantDTO);
+        savedRestaurantMapper.save(savedRestaurant);
+        testRestaurant.increaseSavedCount();
+        restaurantMapper.increaseSavedCount(testRestaurant);
 
         DeleteSavedRestaurantDTO dto = DeleteSavedRestaurantDTO.builder()
                 .memberId(1L)
-                .restaurantId(1L)
+                .restaurantId(testRestaurant.getRestaurantId())
                 .build();
 
         // when
@@ -91,6 +122,33 @@ class SavedRestaurantServiceTest {
 
         // then
         long after = savedRestaurantMapper.findAll().size();
-        assertEquals(before, after);
+        RestaurantDTO actual = restaurantMapper.findById(testRestaurant.getRestaurantId()).get();
+        assertAll(() -> {
+            assertEquals(before, after);
+            assertEquals(testRestaurant.getSavedCount() - 1, actual.getSavedCount());
+        });
+    }
+
+    @Test
+    @DisplayName("식당 저장 값이 0보다 작거나 같을 때 식당 저장을 하면 예외가 발생하는 테스트")
+    void deleteSavedRestaurantWithSavedCountLessOrEqualThan0Test() {
+        // given
+        long before = savedRestaurantMapper.findAll().size();
+
+        SavedRestaurantDTO savedRestaurant = SavedRestaurantDTO.builder()
+                .memberId(1L)
+                .restaurantId(testRestaurant.getRestaurantId())
+                .build();
+        savedRestaurantMapper.save(savedRestaurant);
+
+        DeleteSavedRestaurantDTO dto = DeleteSavedRestaurantDTO.builder()
+                .memberId(1L)
+                .restaurantId(testRestaurant.getRestaurantId())
+                .build();
+
+        // expected
+        assertThatThrownBy(() -> savedRestaurantService.deleteSavedRestaurant(dto))
+                .isInstanceOf(SystemException.class)
+                .hasMessageContaining(NOT_VALID_SAVED_COUNT.getMessage());
     }
 }
